0867c962179ab3c447e2565be4891f22cd6a9b82
Polina Akimenko <polinka_akimenko@mail.ru>
2023-05-30 22:04:11 +0300

    filter ot maxima

---
+++ polina.js
@@ -0,0 +1,11 @@
+const myFilter = (arr, callback) => {
+
+if(!Array.isArray(arr) || callback !== 'function' ) {
+    return []
+}
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        
+    }
+}
